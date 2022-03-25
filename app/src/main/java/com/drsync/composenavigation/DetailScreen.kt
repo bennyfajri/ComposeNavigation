70a05b2c4c95dc88fa7872d26247cbb63ff1135d
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2022-03-25 15:50:29 +0700

    Initial commit

---
+++ app/src/main/java/com/drsync/composenavigation/DetailScreen.kt
@@ -0,0 +1,41 @@
+package com.drsync.composenavigation
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DetailScreen(
+    navController: NavController
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Detail",
+            color = Color.Red,
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.clickable {
+                navController.popBackStack()
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailScreenPreview() {
+    DetailScreen(navController = rememberNavController())
+}
---
+++ app/src/main/java/com/drsync/composenavigation/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.drsync.composenavigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.drsync.composenavigation.ui.theme.ComposeNavigationTheme
+
+class MainActivity : ComponentActivity() {
+
+    lateinit var navController: NavHostController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeNavigationTheme {
+
+                navController = rememberNavController()
+                SetupNavGraph(navHostController = navController)
+
+            }
+        }
+    }
+}
